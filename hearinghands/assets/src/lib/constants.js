1018243f4af5ca61b29a62b04bdc9fbca8d36717
Wondeok Oh <prbs5kong@gmail.com>
2025-01-08 22:34:56 +0900

    readme

--- hearinghands/assets/src/lib/constants.js
+++
@@ -1,3 +0,0 @@
-export const INTERSECTION_OBSERVER_SUPPORTED = "IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype;
-export const CRAWLER = /baidu|(?:google|bing|yandex|duckduck)bot/i.test(navigator.userAgent);
-export const DATA_ATTRS = ["src", "poster"];
--- hearinghands/assets/src/lib/yall-bind-events.js
+++
@@ -1,7 +0,0 @@
-export function yallBindEvents (element, events) {
-  for (const eventIndex in events) {
-    const eventObject = events[eventIndex];
-
-    element.addEventListener(eventIndex, eventObject.listener || eventObject, eventObject.options || undefined);
-  }
-}
